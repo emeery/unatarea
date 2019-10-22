914c7ed9a75bc3a376527abceef5e94fac06cc5d
emeery <gerardoemery@live.com.mx>
2019-10-21 23:40:53 -0500

    leer t&u

--- src/controllers/tareas.js
+++ src/controllers/tareas.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = new express.Router()
+const mongoose = require('mongoose')
 const Tarea = require('../models/tarea')
 router.post('/t', async(req, res) => {
     const t = new Tarea(req.body)
@@ -9,5 +10,28 @@ router.post('/t', async(req, res) => {
     } catch (e) {
         res.status(400).send(e)
     }
+});
+router.get('/ts', async(req, res) => {
+    try {
+        const tsks = await Tarea.find({})
+        res.status(200).send(tsks)
+    } catch (e) {
+        res.status(500).send()
+    }
+});
+router.get('/t/:id', async(req, res) => {
+    const _id = req.params.id
+    try {
+        if (!mongoose.Types.ObjectId.isValid(_id)) {
+            return res.status(404).send()
+        }
+        const t = await Tarea.findById(_id)
+        res.send(t);
+    } catch (e) {
+        res.status(500).send(e)
+    }
+});
+router.patch('', async(req, res) => {
+
 });
 module.exports = router
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = new express.Router()
 const User = require('../models/user')
+const mongoose = require('mongoose')
 router.post('/u', async(req, res) => {
     const me = new User(req.body)
     try {
@@ -9,5 +10,25 @@ router.post('/u', async(req, res) => {
     } catch (e) {
         res.status(400).send(e)
     }
-});
+})
+router.get('/us', async(req, res) => {
+    try {
+        const us = await User.find({})
+        res.send(us)
+    } catch (e) {
+        res.send(500).send()
+    }
+})
+router.get('/u/:id', async(req, res) => {
+    const _id = req.params.id;
+    try {
+        if (!mongoose.Types.ObjectId.isValid(_id)) {
+            return res.status(404).send()
+        }
+        const user = await User.findById(_id)
+        res.send(user);
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
 module.exports = router
